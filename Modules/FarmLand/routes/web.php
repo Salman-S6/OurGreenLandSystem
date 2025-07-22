454a4c0d49aef1dd770a19aa7af8d4c916aacd13
Salman-S6 <salmansumak24@gmail.com>
2025-07-22 23:01:53 +0300

    intt translate

--- Modules/FarmLand/routes/web.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use Modules\FarmLand\Http\Controllers\FarmLandController;
-
-Route::middleware(['auth', 'verified'])->group(function () {
-    Route::resource('farmlands', FarmLandController::class)->names('farmland');
-});
--- Modules/Farmland/routes/api.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use Modules\FarmLand\Http\Controllers\FarmLandController;
-
-Route::middleware(['auth:sanctum'])->prefix('v1')->group(function () {
-    Route::apiResource('farmlands', FarmLandController::class)->names('farmland');
-});
