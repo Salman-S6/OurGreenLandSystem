315a7df50fadcd700cf8669a903fcb96766bcc61
Maryam Asha <ashamaryam84@gmail.com>
2025-07-26 03:38:02 +0300

    Adding Resources Modoule file

---
+++ Modules/Resources/app/Http/Controllers/Api/V1/SupplierController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace  Modules\Resources\app\Http\Controllers\Api\V1;
+
+use App\Http\Controllers\Controller;
+
+use App\Http\Requests\Supplier\StoreSupplierRequest;
+use App\Http\Requests\Supplier\UpdateSupplierRequest;
+use App\Models\Supplier;
+
+class SupplierController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreSupplierRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Supplier $supplier)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateSupplierRequest $request, Supplier $supplier)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Supplier $supplier)
+    {
+        //
+    }
+}
---
+++ Modules/Resources/app/Policies/SupplierRatingPolicy.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Modules\Resources\app\Policies;
+
+use App\Models\SupplierRating;
+use App\Models\User;
+use Illuminate\Auth\Access\Response;
+
+class SupplierRatingPolicy
+{
+    /**
+     * Determine whether the user can view any models.
+     */
+    public function viewAny(User $user): bool
+    {
+        return false;
+    }
+
+    /**
+     * Determine whether the user can view the model.
+     */
+    public function view(User $user, SupplierRating $supplierRating): bool
+    {
+        return false;
+    }
+
+    /**
+     * Determine whether the user can create models.
+     */
+    public function create(User $user): bool
+    {
+        return false;
+    }
+
+    /**
+     * Determine whether the user can update the model.
+     */
+    public function update(User $user, SupplierRating $supplierRating): bool
+    {
+        return false;
+    }
+
+    /**
+     * Determine whether the user can delete the model.
+     */
+    public function delete(User $user, SupplierRating $supplierRating): bool
+    {
+        return false;
+    }
+
+}
