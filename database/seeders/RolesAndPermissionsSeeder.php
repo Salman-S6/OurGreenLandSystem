237ffa5581c111a1ffdf122e01789842589b4f01
eyadswadi5 <eyadswadi5@gmail.com>
2025-08-02 21:39:08 +0300

    fix(Auth): updates on AuthorizationController for better performance, updates on authorization requests.

--- database/seeders/RolesAndPermissionsSeeder.php
+++ database/seeders/RolesAndPermissionsSeeder.php
@@ -3,8 +3,8 @@
 namespace Database\Seeders;
 
 use App\Enums\UserRoles;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Route;
 use Spatie\Permission\Models\Permission;
 use Spatie\Permission\Models\Role;
 use Spatie\Permission\PermissionRegistrar;
