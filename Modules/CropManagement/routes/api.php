5f9ee653de6a44638453a42a0fb5c1943d2fec7c
Ahmad Shehade <std.ahmad.shehade@gmail.com>
2025-08-05 14:13:54 +0300

    Refactor and update API route definitions in CropManagement and Resources modules

--- Modules/CropManagement/routes/api.php
+++ Modules/CropManagement/routes/api.php
@@ -6,42 +6,91 @@ use Modules\CropManagement\Http\Controllers\Api\V1\CropPlanController;
 use Modules\CropManagement\Http\Controllers\Api\V1\PestDiseaseCaseController;
 use Modules\CropManagement\Http\Controllers\Api\V1\ProductionEstimationController;
 
-Route::middleware(['auth:sanctum',"throttle:api"])->group(function () {
+Route::middleware(['auth:sanctum', "throttle:api"])->group(function () {
 
+    /**
+     *
+     *
+     * ----------------------------------------------------------------
+     * CropContoller routes.
+     * ----------------------------------------------------------------
+     */
 
     Route::prefix('crops')->group(function () {
+
         Route::post('/create', [CropController::class, 'store'])->name('crop.create');
+
         Route::post('/update/{crop}', [CropController::class, 'update'])->name('crop.update');
+
         Route::get('/get-all', [CropController::class, 'index'])->name('crops.index');
+
         Route::get('/{crop}', [CropController::class, 'show'])->name('crops.show');
+
         Route::delete('/{crop}', [CropController::class, 'destroy'])->name('crop.delete');
     });
 
-
+    /**
+     *
+     *
+     * ----------------------------------------------------------------
+     * CropPlanController routes.
+     * ----------------------------------------------------------------
+     */
 
     Route::prefix('cropPlan')->group(function () {
+
         Route::post('/create', [CropPlanController::class, 'store'])->name('cropPlan.make');
+
         Route::post('/update/{cropPlan}', [CropPlanController::class, 'update'])->name('cropPlan.update');
+
         Route::get('/{cropPlan}', [CropPlanController::class, 'show'])->name('cropPlan.show');
+
         Route::get('/all/paln', [CropPlanController::class, 'index'])->name('cropPaln.all');
+
         Route::post('/change-to-cancelled/{cropPlan}', [CropPlanController::class, 'switchStatusToCancelled'])->name('cropPlan.cancelled');
+
         Route::delete('/{cropPlan}', [CropPlanController::class, 'destroy'])->name('cropPlan.destroy');
     });
 
+    /**
+     *
+     *
+     * ----------------------------------------------------------------
+     * ProductionEstimationController routes.
+     * ----------------------------------------------------------------
+     */
 
     Route::prefix('production-estimation')->group(function () {
+
         Route::post('/create', [ProductionEstimationController::class, 'store'])->name('estimation.create');
+
         Route::post('/update/{productionEstimation}', [ProductionEstimationController::class, 'update'])->name('estimation.update');
+
         Route::get('/get/{productionEstimation}', [ProductionEstimationController::class, 'show'])->name('estimation.show');
+
         Route::get('/all', [ProductionEstimationController::class, 'index'])->name('estimation.all');
+
         Route::delete('/{productionEstimation}', [ProductionEstimationController::class, 'destroy'])->name('estimation.delete');
     });
 
+    /**
+     *
+     *
+     * ----------------------------------------------------------------
+     * PestDiseaseCaseController routes.
+     * ----------------------------------------------------------------
+     */
+
     Route::prefix('disease-case')->group(function () {
+
         Route::post('/create', [PestDiseaseCaseController::class, 'store'])->name('case.make');
+
         Route::post('/update/{pestDiseaseCase}', [PestDiseaseCaseController::class, 'update'])->name('case.update');
+
         Route::get('/get-all', [PestDiseaseCaseController::class, 'index'])->name('case.all');
+
         Route::get('/get/{pestDiseaseCase}', [PestDiseaseCaseController::class, 'show'])->name('case.get');
+        
         Route::delete('/{pestDiseaseCase}', [PestDiseaseCaseController::class, 'destroy'])->name('case.delete');
     });
 });
--- Modules/Resources/routes/api.php
+++ Modules/Resources/routes/api.php
@@ -4,12 +4,23 @@ use Illuminate\Support\Facades\Route;
 use Modules\Resources\Http\Controllers\Api\V1\InputRequestController;
 use Modules\Resources\Http\Controllers\ResourcesController;
 
-Route::middleware(['auth:sanctum','throttle:api'])->prefix('input-request')->group(function () {
- 
- Route::post('/create',[InputRequestController::class,'store'])->name('request.store');
-  Route::post('/update/{inputRequest}',[InputRequestController::class,'update'])->name('request.update');
-  Route::get('/get/{inputRequest}',[InputRequestController::class,'show'])->name('request.show');
-  Route::get('/all',[InputRequestController::class,'index'])->name('request.all');
-  Route::delete('/{inputRequest}',[InputRequestController::class,'destroy'])->name('request.delete');
+Route::middleware(['auth:sanctum', 'throttle:api'])->prefix('input-request')->group(function () {
 
+  /**
+   *
+   *
+   * ----------------------------------------------------------------
+   * InputRequestController routes.
+   * ----------------------------------------------------------------
+   */
+
+  Route::post('/create', [InputRequestController::class, 'store'])->name('request.store');
+
+  Route::post('/update/{inputRequest}', [InputRequestController::class, 'update'])->name('request.update');
+
+  Route::get('/get/{inputRequest}', [InputRequestController::class, 'show'])->name('request.show');
+
+  Route::get('/all', [InputRequestController::class, 'index'])->name('request.all');
+
+  Route::delete('/{inputRequest}', [InputRequestController::class, 'destroy'])->name('request.delete');
 });
