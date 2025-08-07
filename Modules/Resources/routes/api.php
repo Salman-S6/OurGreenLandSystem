7a1eaeea35c5e5399f8ad8186d9daa4fb8624084
Maryam Asha <ashamaryam84@gmail.com>
2025-08-07 22:09:38 +0300

    fix(Resources Module): change routes/ api

--- Modules/Resources/routes/api.php
+++ Modules/Resources/routes/api.php
@@ -7,7 +7,8 @@ use Modules\Resources\Http\Controllers\Api\V1\InputRequestController;
 use Modules\Resources\Http\Controllers\ResourcesController;
 
 
-Route::prefix('resources')->middleware(['auth:sanctum'])->group(function () {
+Route::prefix('resources')->middleware(['auth:sanctum', 'throttle:api'])->group(function () {
+
     Route::apiResource('suppliers', SupplierController::class)->names('resources.suppliers');
     Route::get('supplier-ratings', [SupplierRatingController::class, 'index'])->name('resources.supplier-ratings.index');
     Route::post('supplier-ratings', [SupplierRatingController::class, 'store'])->name('resources.supplier-ratings.store');
@@ -15,25 +16,27 @@ Route::prefix('resources')->middleware(['auth:sanctum'])->group(function () {
     Route::get('supplier-ratings/{rating}', [SupplierRatingController::class, 'show'])->name('resources.supplier-ratings.show');
     Route::put('supplier-ratings/{rating}', [SupplierRatingController::class, 'update'])->name('resources.supplier-ratings.update');
     Route::delete('supplier-ratings/{rating}', [SupplierRatingController::class, 'destroy'])->name('resources.supplier-ratings.destroy');
-});
 
-Route::middleware(['auth:sanctum', 'throttle:api'])->prefix('input-request')->group(function () {
 
-    /**
-     *
-     *
-     * ----------------------------------------------------------------
-     * InputRequestController routes.
-     * ----------------------------------------------------------------
-     */
 
-    Route::post('/create', [InputRequestController::class, 'store'])->name('request.store');
+    Route::prefix('input-request')->group(function () {
 
-    Route::post('/update/{inputRequest}', [InputRequestController::class, 'update'])->name('request.update');
+        /**
+         *
+         *
+         * ----------------------------------------------------------------
+         * InputRequestController routes.
+         * ----------------------------------------------------------------
+         */
 
-    Route::get('/get/{inputRequest}', [InputRequestController::class, 'show'])->name('request.show');
+        Route::post('/create', [InputRequestController::class, 'store'])->name('request.store');
 
-    Route::get('/all', [InputRequestController::class, 'index'])->name('request.all');
+        Route::post('/update/{inputRequest}', [InputRequestController::class, 'update'])->name('request.update');
 
-    Route::delete('/{inputRequest}', [InputRequestController::class, 'destroy'])->name('request.delete');
+        Route::get('/get/{inputRequest}', [InputRequestController::class, 'show'])->name('request.show');
+
+        Route::get('/all', [InputRequestController::class, 'index'])->name('request.all');
+
+        Route::delete('/{inputRequest}', [InputRequestController::class, 'destroy'])->name('request.delete');
+    });
 });
